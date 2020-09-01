ecd1ad95cd3d3f74872f09bd6b68bd4f4ac929c3
Lilly Witschel <witschel@mogic.com>
2020-09-01 15:55:24 +0200

    close

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function myFunction() {
     let element = document.getElementById("list");
-    element.classList.toggle("invisible")
+    element.classList.toggle("invisible");
+
+    let element2 = document.getElementById("menu");
+    element2.classList.toggle("invisible");
+
+    let element3 = document.getElementById("close");
+    element3.classList.toggle("invisible");
   }
